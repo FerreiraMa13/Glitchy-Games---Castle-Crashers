78052b3732007716498e6b8be220271aa75e6950
Callum <callumd305@icloud.com>
2022-10-08 12:14:10 +0100

    Pickup basic done

---
+++ Castle Crashers/Assets/Scripts/projectiles.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectiles : MonoBehaviour
+{
+    public float speed = 4.5f;
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
